b9fbf5e3b56ba0121447cef663dbd50989670d0c
Davide Vacca <davide.vacca@onfido.com>
2025-06-13 15:01:45 +0200

    Add a test to validate that retrieved SDK token actually works

--- src/test/java/com/onfido/integration/SdkTokenTest.java
+++ src/test/java/com/onfido/integration/SdkTokenTest.java
@@ -3,6 +3,7 @@ package com.onfido.integration;
 import com.onfido.model.Applicant;
 import com.onfido.model.SdkToken;
 import com.onfido.model.SdkTokenBuilder;
+import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,16 @@ public class SdkTokenTest extends TestBase {
     Assertions.assertTrue(token.getToken().length() > 0);
 
     Assertions.assertNotNull(token.toJson());
+
+    // Smoke test to detect any authentication issues
+    java.net.URL url =
+        new java.net.URL("https://api.onfido.com/v3.6/workflow_runs/" + UUID.randomUUID());
+    java.net.HttpURLConnection connection = (java.net.HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("GET");
+    connection.setRequestProperty("Authorization", "Bearer " + token.getToken());
+    connection.setRequestProperty("Content-Type", "application/json");
+
+    Assertions.assertNotEquals(
+        401, connection.getResponseCode(), "Authentication error occurred with the token");
   }
 }

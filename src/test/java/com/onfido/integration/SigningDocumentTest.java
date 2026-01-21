cafb0193efb7ebe9a4b9bc5629fb73b9bb5cc826
Davide Vacca <davide.vacca@onfido.com>
2026-01-21 09:51:49 +0100

    Make linter happy again

--- src/test/java/com/onfido/integration/SigningDocumentTest.java
+++ src/test/java/com/onfido/integration/SigningDocumentTest.java
@@ -55,7 +55,7 @@ public class SigningDocumentTest extends TestBase {
   @Test
   public void listSigningDocumentsTest() throws Exception {
     List<SigningDocument> documents =
-      onfido.listSigningDocuments(applicant.getId()).getSigningDocuments();
+        onfido.listSigningDocuments(applicant.getId()).getSigningDocuments();
 
     Assertions.assertNotNull(documents);
     Assertions.assertTrue(
